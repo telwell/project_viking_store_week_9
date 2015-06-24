ace37b12cefd63e8b37ccc47a9f1b1377b5e4ac6
telwell <me@trevorelwell.com>
2015-06-23 23:46:50 -0400

    Update/Edit category complete

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -25,4 +25,21 @@ class CategoriesController < ApplicationController
 	def show
 		@category = Category.find(id = params[:id])
 	end
+
+	def edit
+		@category = Category.find(id = params[:id])
+	end
+
+	def update
+		@category = Category.find(id = params[:id])
+		@category.name = params[:category][:name]
+		@category.description = params[:category][:description]
+		if @category.save
+			flash[:success] = "Category successfully updated!"
+			redirect_to categories_path
+		else
+			flash.now[:error] = "There were problems with your update, please fix"
+			render :edit
+		end
+	end
 end
