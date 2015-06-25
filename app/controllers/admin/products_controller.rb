7ba6a654699950401dd5b551745e3a229c936503
telwell <me@trevorelwell.com>
2015-06-24 21:38:12 -0400

    Show product complete

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -5,4 +5,15 @@ class Admin::ProductsController < ApplicationController
 		@products = Product.all
 	end
 
+	def show
+		@product = Product.find(id = params[:id])
+		@category = category_name(@product)
+		@orders = @product.orders.where("checkout_date IS NOT null").count
+		@carts = @product.orders.where("checkout_date IS null").count
+	end
+
+private 
+	def category_name(product)
+		product.category ? product.category.name : "Category not set"
+	end
 end
