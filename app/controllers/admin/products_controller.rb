be635f45eec8d1ae2ddb33345b9d56976b93739a
telwell <me@trevorelwell.com>
2015-06-24 23:36:24 -0400

    creating a new product complete

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -12,8 +12,31 @@ class Admin::ProductsController < ApplicationController
 		@carts = @product.orders.where("checkout_date IS null").count
 	end
 
-private 
+	def new
+		@product = Product.new
+		@categories = Category.all
+	end
+
+	def create
+		@product = Product.new(whitelisted_product_params)
+		@categories = Category.all
+		# This below is to make sure that our category is actually in the list of categories
+		if @categories.map{|category| category.id }.include?(params[:product][:category_id].to_i) && @product.save 
+			flash[:success] = "Product created successfully!"
+			redirect_to admin_products_path
+		else
+			render :new
+		end
+	end
+
+private
+	# Since a product may not have a category I wanted to add a default 
+	# in case there was nothing set. 
 	def category_name(product)
 		product.category ? product.category.name : "Category not set"
+	end
+
+	def whitelisted_product_params
+		params.require(:product).permit(:name, :sku, :description, :price, :category_id)
 	end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,9 @@ class Product < ActiveRecord::Base
 	has_many :order_contents, class_name: "OrderContents"
 	has_many :users, through: :orders
 
+	validates :name, :sku, :description, :price, :category_id, :presence => true
+	validates :price, :numericality => { :less_than_or_equal_to => 10_000 }
+
 	def self.total_product
 		# Allow us to pass in a block which will get the rows in a 
 		# particular timeframe.
